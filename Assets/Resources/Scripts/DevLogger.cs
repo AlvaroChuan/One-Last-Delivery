5c37564a26be5ee1305e76d488c64dd47b76ebd2
Diego Pérez <dperezrueda22@gmail.com>
2026-06-11 20:10:03 +0200

    added filter to devlogger

--- Assets/Resources/Scripts/DevLogger.cs
+++ Assets/Resources/Scripts/DevLogger.cs
@@ -3,26 +3,47 @@ using System.Diagnostics;
 public static class DevLogger
 {
     [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
-    public static void Log(string message, [System.Runtime.CompilerServices.CallerFilePath] string caller = "")
+    public static void Log(string message, string filter = "", [System.Runtime.CompilerServices.CallerFilePath] string caller = "")
     {
 #if UNITY_EDITOR || DEVELOPMENT_BUILD
         string callerName = System.IO.Path.GetFileNameWithoutExtension(caller);
-        UnityEngine.Debug.Log($"[{callerName}] {message}");
+        if (string.IsNullOrEmpty(filter))
+        {
+            UnityEngine.Debug.Log($"[{callerName}] {message}");
+        }
+        else
+        {
+            UnityEngine.Debug.Log($"[{filter}] [{callerName}] {message}");
+        }
 #endif
     }
 
     [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
-    public static void LogWarning(string message, [System.Runtime.CompilerServices.CallerFilePath] string caller = "")
+    public static void LogWarning(string message, string filter = "", [System.Runtime.CompilerServices.CallerFilePath] string caller = "")
     {
 #if UNITY_EDITOR || DEVELOPMENT_BUILD
         string callerName = System.IO.Path.GetFileNameWithoutExtension(caller);
-        UnityEngine.Debug.LogWarning($"[{callerName}] {message}");
+        if (string.IsNullOrEmpty(filter))
+        {
+            UnityEngine.Debug.LogWarning($"[{callerName}] {message}");
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning($"[{filter}] [{callerName}] {message}");
+        }
 #endif
     }
 
-    public static void LogError(string message, [System.Runtime.CompilerServices.CallerFilePath] string caller = "")
+    public static void LogError(string message, string filter = "", [System.Runtime.CompilerServices.CallerFilePath] string caller = "")
     {
         string callerName = System.IO.Path.GetFileNameWithoutExtension(caller);
-        UnityEngine.Debug.LogError($"[{callerName}] {message}");
+        if (string.IsNullOrEmpty(filter))
+        {
+            UnityEngine.Debug.LogError($"[{callerName}] {message}");
+        }
+        else
+        {
+            UnityEngine.Debug.LogError($"[{filter}] [{callerName}] {message}");
+        }
     }
 }
