8973bfee2afa3dca9460df31918fbe9c77da26ab
Diego Pérez <dperezrueda22@gmail.com>
2026-06-24 18:52:53 +0200

    Update TruckSeat.cs

--- Assets/Resources/Scripts/Truck/TruckSeat.cs
+++ Assets/Resources/Scripts/Truck/TruckSeat.cs
@@ -3,6 +3,7 @@ using Mirror;
 using UnityEngine.InputSystem;
 using Unity.VisualScripting;
 using System.Collections.Generic;
+using System;
 
 public class TruckSeat : Interactable
 {
@@ -10,34 +11,51 @@ public class TruckSeat : Interactable
     [SerializeField] Transform _exitPosition;
     [SerializeField] bool _isDriverSeat = true;
     [SerializeField] InputActionReference _getUpInputActionReference;
+    [SyncVar(hook = nameof(OnOccupantChanged))]
     GameObject _occupant;
     Quaternion _lastRotation;
+
+    private void OnOccupantChanged(GameObject oldOccupant, GameObject newOccupant)
+    {
+        if (isServer)
+        {
+            if (newOccupant != null)
+            {
+                netIdentity.RemoveClientAuthority();
+                netIdentity.AssignClientAuthority(newOccupant.GetComponent<NetworkIdentity>().connectionToClient);
+            }
+            else
+            {
+                netIdentity.RemoveClientAuthority();
+            }
+        }
+
+        if (oldOccupant != null)
+        {
+            if(NetworkClient.connection.identity != null && oldOccupant == NetworkClient.connection.identity.gameObject)
+            {
+                // If the old occupant is the local player, re-enable their input and colliders
+                SetPlayerInput(oldOccupant, false);
+            }
+            SetPlayerCollidersEnabled(oldOccupant, false); // Re-enable colliders for the old occupant
+        }
+        if (newOccupant != null)
+        {
+            if(NetworkClient.connection.identity != null && newOccupant == NetworkClient.connection.identity.gameObject)
+            {
+                // If the new occupant is the local player, disable their input and colliders
+                SetPlayerInput(newOccupant, true);
+            }
+            SetPlayerCollidersEnabled(newOccupant, true); // Disable colliders for the new occupant
+            netIdentity.AssignClientAuthority(newOccupant.GetComponent<NetworkIdentity>().connectionToClient);
+        }
+    }
+
     public override void ServerInteract(GameObject interactor)
     {
         if (!_isDriverSeat) return; // Seat is already occupied
 
-        NetworkIdentity interactorIdentity = interactor.GetComponent<NetworkIdentity>();
-
-        netIdentity.RemoveClientAuthority();
-        netIdentity.AssignClientAuthority(interactorIdentity.connectionToClient);
-    }
-
-    public override void ClientInteraction(GameObject interactor)
-    {
-        if (_occupant != null) return; // Seat is already occupied
-
         _occupant = interactor;
-        _occupant.transform.position = _occupantPosition.position;
-
-        _getUpInputActionReference.action.Enable();
-        _getUpInputActionReference.action.performed += OnGetUpPerformed;
-
-        if (_occupant.TryGetComponent<PlayerInteractComponent>(out var playerInteractComponent))
-        {
-            playerInteractComponent.onInteractEvent += OnPlayerInteract;
-        }
-
-        SetPlayerInputEnabled(_occupant, false); // Disable player input while seated
     }
 
     private void OnPlayerInteract(PlayerInteractComponent.InteractInfo info)
@@ -60,30 +78,54 @@ public class TruckSeat : Interactable
         _lastRotation = transform.rotation;
     }
 
-    void SetPlayerInputEnabled(GameObject player, bool enabled)
+    void SetPlayerInput(GameObject player, bool isOnTruck)
     {
         if (player.TryGetComponent<PlayerMovementComponent>(out var playerMovementComponent))
         {
-            playerMovementComponent.enabled = enabled;
+            playerMovementComponent.enabled = !isOnTruck;
         }
-        if (player.TryGetComponent<PlayerJumpComponent>(out var playerInteractComponent))
+        if (player.TryGetComponent<PlayerJumpComponent>(out var playerJumpComponent))
         {
-            playerInteractComponent.enabled = enabled;
+            playerJumpComponent.enabled = !isOnTruck;
         }
         if (_isDriverSeat && player.TryGetComponent<PlayerItemUseComponent>(out var playerItemUseComponent))
         {
-            playerItemUseComponent.enabled = enabled;
+            playerItemUseComponent.enabled = !isOnTruck;
         }
         if (_isDriverSeat && player.TryGetComponent<PlayerInventoryComponent>(out var playerInventoryComponent))
         {
-            playerInventoryComponent.enabled = enabled;
+            playerInventoryComponent.enabled = !isOnTruck;
         }
-
-        List<Collider> playerColliders = new List<Collider>(_occupant.GetComponents<Collider>());
-        playerColliders.AddRange(_occupant.GetComponentsInChildren<Collider>());
-        foreach (var collider in playerColliders)
+        if(isOnTruck)
         {
-            collider.enabled = enabled; // Re-enable the player's colliders
+            _getUpInputActionReference.action.performed += OnGetUpPerformed;
+            _getUpInputActionReference.action.Enable();
+
+            PlayerInteractComponent playerInteractComponent = player.GetComponent<PlayerInteractComponent>();
+            if (playerInteractComponent != null)
+            {
+                playerInteractComponent.onInteractEvent += OnPlayerInteract;
+            }
+        }
+        else
+        {
+            _getUpInputActionReference.action.performed -= OnGetUpPerformed;
+            _getUpInputActionReference.action.Disable();
+
+            PlayerInteractComponent playerInteractComponent = player.GetComponent<PlayerInteractComponent>();
+            if (playerInteractComponent != null)
+            {
+                playerInteractComponent.onInteractEvent -= OnPlayerInteract;
+            }
+        }
+    }
+
+    void SetPlayerCollidersEnabled(GameObject player, bool isOnTruck)
+    {
+        Collider[] colliders = player.GetComponentsInChildren<Collider>();
+        foreach (var collider in colliders)
+        {
+            collider.enabled = !isOnTruck;
         }
     }
 
@@ -91,36 +133,11 @@ public class TruckSeat : Interactable
     {
         if (_occupant == null) return; // No occupant to get up
 
-        SetPlayerInputEnabled(_occupant, true); // Re-enable player input
         GetUp();
-
-        if (_isDriverSeat)
-        {
-            CmdOnGetUp();
-        }
     }
 
     void GetUp()
     {
-        PlayerInteractComponent playerInteractComponent = _occupant.GetComponent<PlayerInteractComponent>();
-        if (playerInteractComponent != null)
-        {
-            playerInteractComponent.onInteractEvent -= OnPlayerInteract;
-        }
-
-        _occupant.transform.position = _exitPosition.position;
-        _occupant.transform.rotation = _exitPosition.rotation;
-        _occupant.transform.up = Vector3.up; // Ensure the player is upright when getting up
-
         _occupant = null;
-
-        _getUpInputActionReference.action.performed -= OnGetUpPerformed;
-        _getUpInputActionReference.action.Disable();
-    }
-
-    [Command]
-    void CmdOnGetUp()
-    {
-        netIdentity.RemoveClientAuthority();
     }
 }
