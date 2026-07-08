efcc6c4284564b404508b9a9ccd263dbe7ae2ffe
Diego Pérez <dperezrueda22@gmail.com>
2026-07-08 21:54:15 +0200

    step sounds

--- Assets/Resources/Scripts/Enemies/BasicEnemy/BasicEnemy.cs
+++ Assets/Resources/Scripts/Enemies/BasicEnemy/BasicEnemy.cs
@@ -40,10 +40,6 @@ public class BasicEnemy : NetworkBehaviour
         _wanderBehaviour.StartWander();
     }
 
-    void OnEnable()
-    {
-    }
-
     void OnDisable()
     {
         _attackComponent.onAttackStartedEvent -= OnAttackStarted;
