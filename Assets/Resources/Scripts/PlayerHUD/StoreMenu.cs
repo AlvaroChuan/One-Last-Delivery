9db75a00bdbd52a4504fb31e79766d12210a63cb
Diego Pérez <dperezrueda22@gmail.com>
2026-07-08 19:51:50 +0200

    Update StoreMenu.cs

--- Assets/Resources/Scripts/PlayerHUD/StoreMenu.cs
+++ Assets/Resources/Scripts/PlayerHUD/StoreMenu.cs
@@ -11,25 +11,10 @@ public class StoreMenu : MonoBehaviour
     [SerializeField] private Transform _truckUpgradeButtonContainer;
     [Header("Prefabs")]
     [SerializeField] private PurchaseButton _purchaseButtonPrefab;
-    bool _storeAvailable = true;
 
     private void Start()
     {
         PopulateStore();
-        SunManager.OnNightfall += OnNightfall;
-    }
-
-    void OnDestroy()
-    {
-        SunManager.OnNightfall -= OnNightfall;
-    }
-
-    void OnEnable()
-    {
-        if (!_storeAvailable)
-        {
-            gameObject.SetActive(false);
-        }
     }
 
     void PopulateStore()
@@ -46,16 +31,6 @@ public class StoreMenu : MonoBehaviour
         {
             var button = Instantiate(_purchaseButtonPrefab, _truckUpgradeButtonContainer);
             button.SetListing(truckUpgrade);
-        }
-    }
-
-    public void OnNightfall()
-    {
-        _storeAvailable = false;
-        // Optionally, you can disable the store UI or show a message indicating that the store is closed.
-        if (gameObject.activeSelf)
-        {
-            gameObject.SetActive(false);
         }
     }
 }
