2f75fd4738639e5f17c28a38c240e6fa9b2d80fe
Diego Pérez <dperezrueda22@gmail.com>
2026-07-08 16:11:27 +0200

    added global cooldown to creepy sound

--- Assets/Resources/Scripts/Enemies/EnemySpottedSound.cs
+++ Assets/Resources/Scripts/Enemies/EnemySpottedSound.cs
@@ -2,17 +2,19 @@ using UnityEngine;
 using Mirror;
 
 [RequireComponent(typeof(FieldOfViewDetector))]
-public class CreepySound : NetworkBehaviour
+public class EnemyDetectionSound : MonoBehaviour
 {
     [SerializeField] private AudioEvent _creepySound;
     [SerializeField] private float _detectionRange = 20f; // Range within which the sound can be heard
     [SerializeField] private float _detectionInterval = 0.5f; // Interval at which to check for players in range
     [SerializeField] private float _detectionRefreshTime = 30f; // Time after which the detection state resets if no players are detected
+    private static float GlobalCooldown = 5f; // Global cooldown for the sound to prevent spamming
     FieldOfViewDetector _fovDetector;
     float _detectionTimer = 0f;
     bool _isPlayerDetected = false;
     bool _hasPlayedSound = false;
     float _detectionRefreshTimer = 0f;
+    private static float LastSoundTime = -Mathf.Infinity; // Track the last time the sound was played globally
 
     void Awake()
     {
@@ -37,8 +39,16 @@ public class CreepySound : NetworkBehaviour
         _isPlayerDetected = _fovDetector.IsInFOV(_detectionRange);
         if (_isPlayerDetected && !_hasPlayedSound)
         {
-            _creepySound.Play(transform.position); // Play the creepy sound at the object's position
-            _hasPlayedSound = true; // Mark that the sound has been played to avoid repetition
+            if (Time.time - LastSoundTime >= GlobalCooldown)
+            {
+                _creepySound.Play(transform.position); // Play the creepy sound at the object's position
+                _hasPlayedSound = true; // Mark that the sound has been played to avoid repetition
+                LastSoundTime = Time.time; // Update the last sound time
+            }
+            else
+            {
+                _hasPlayedSound = true; // Mark that the sound has been played to avoid repetition, even if it wasn't actually played due to cooldown
+            }
         }
     }
     void CheckRefresh()
