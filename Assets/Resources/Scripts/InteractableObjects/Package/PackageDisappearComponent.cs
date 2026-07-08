918d73aa8e4b053dd4c5b9816e7e2509afdddc34
Diego Pérez <dperezrueda22@gmail.com>
2026-07-08 18:26:35 +0200

    added netrigidbodycontrolelr to truck

--- Assets/Resources/Scripts/InteractableObjects/Package/PackageDisappearComponent.cs
+++ Assets/Resources/Scripts/InteractableObjects/Package/PackageDisappearComponent.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class PackageDisappearComponent : NetworkBehaviour
 {
-    [SerializeField] private float _fadeDuration = 1f;
+    [SerializeField] ParticleSystem _disappearEffect;
 
 #if UNITY_EDITOR
     [ContextMenu("Disappear Package")]
@@ -17,48 +17,8 @@ public class PackageDisappearComponent : NetworkBehaviour
     [Server]
     public void StartDisappear()
     {
-        RpcDisappear();
-        Invoke(nameof(DestroyPackage), _fadeDuration);
-    }
-
-    void DestroyPackage()
-    {
+        Instantiate(_disappearEffect, transform.position, Quaternion.identity);
         NetworkServer.Destroy(gameObject);
-    }
-
-    [ClientRpc]
-    public void RpcDisappear()
-    {
-        StartCoroutine(DisappearCoroutine());
-    }
-
-    IEnumerator DisappearCoroutine()
-    {
-        Renderer[] renderers = GetComponentsInChildren<Renderer>();
-
-        float elapsedTime = 0.0f;
-
-        while (elapsedTime < _fadeDuration)
-        {
-            float fadeValue = Mathf.Lerp(0.0f, 1.0f, elapsedTime / _fadeDuration);
-            foreach (var renderer in renderers)
-            {
-                foreach (var material in renderer.materials)
-                {
-                    material.SetFloat("_FadeValue", fadeValue);
-                }
-            }
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        // Ensure the final value is set to 1.0f
-        foreach (var renderer in renderers)
-        {
-            foreach (var material in renderer.materials)
-            {
-                material.SetFloat("_FadeValue", 1.0f);
-            }
-        }
+        NetworkServer.Spawn(_disappearEffect.gameObject);
     }
 }
